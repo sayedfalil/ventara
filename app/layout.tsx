30da9ae46043329ceba1e2cb3bb42dec4c0174e4
sayed falil <sayedfalil@sayeds-MacBook-Air.local>
2026-03-23 12:52:47 +0530

    google anlaytics integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +14,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-EBKRK5N95G" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-EBKRK5N95G');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
